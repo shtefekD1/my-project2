0cfe316f41fa402bd4b62425e2a2517f031920f4
Stefan Bajic <shtefeksrb@icloud.com>
2025-02-15 17:17:35 +0100

    Update Main04.java

--- src/main/java/pl/coderslab/JavaExercisesDayOneBasics/methods/Main04.java
+++ src/main/java/pl/coderslab/JavaExercisesDayOneBasics/methods/Main04.java
@@ -3,8 +3,7 @@ package pl.coderslab.JavaExercisesDayOneBasics.methods;
 public class Main04 {
     public static void main(String[] args) {
         // Example usage
-        System.out.println(createName("Stefan", "Bajic", "shtefek")); // Output: John Johnny Doe
-    }
+        System.out.println(createName("Stefan", "Bajic", "shtefek")); // Output: Stefan shtefek Bajic
 
     public static String createName(String name, String surname, String nickname) {
         return name + " " + nickname + " " + surname;
