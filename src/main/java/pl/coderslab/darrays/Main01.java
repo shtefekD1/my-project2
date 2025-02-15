25d7dd46a4e358ad817df43c4638a97848638f03
Stefan Bajic <shtefeksrb@icloud.com>
2025-02-15 16:23:11 +0100

    Delete src/main/java/pl/coderslab/darrays directory

--- src/main/java/pl/coderslab/darrays/Main01.java
+++
@@ -1,9 +0,0 @@
-package pl.coderslab.darrays;
-
-public class Main01 {
-    public static void main(String[] args) {
-        int[] threeElements = {3, 4, 5};
-        System.out.print(threeElements[0] + " " + threeElements[1] + " " + threeElements[2]);
-    }
-}
-
